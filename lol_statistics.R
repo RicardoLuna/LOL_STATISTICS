929e41c35afd26f715f1b1e0d0b651c3e7b6da11
Iverson <ilp_1995@hotmail.com>
2018-07-19 00:21:27 -0300

    created file with champions name, added new statistic: most popular champ, and started structure to create statistic per region

--- lol_statistics.R
+++ lol_statistics.R
@@ -1,36 +1,39 @@
+################################################
+# Universidade Federal Rural de Pernambuco     #
+# Disciplina: Computação para Análise de Dados #
+# Professor: Ermerson Andrade                  #
+# Alunos: Iverson Pereira                      #
+#         Ricardo Luna                         #
+################################################
+
+
+# use this if you need to install the packages
+install.packages('ggplot2')
+install.packages('tidyr')
+install.packages('dplyr')
+
+# put here the libraries
 library(ggplot2)
-work_station<-'C:/Users/ricardo/Documents/CAD'
+library(tidyr)
+library(dplyr)
+
+# set here your work station
+work_station<-'/home/iverson/git/LOL_STATISTICS/'
 setwd(work_station)
 
-library(tidyr)
-champ_names<-c( 'Aatrox','Ahri','Akali','Alistar','Amumu','Anivia','Annie','Ashe',
-                'Aurelion Sol', 'Azir', 'Bard','Blitzcrank','Brand','Braum','Caitlyn','Camille',
-                'Cassiopeia',"Cho'Gath",'Corki','Darius','Diana','Dr. Mundo','Draven','Ekko',
-                'Elise','Evelynn','Ezreal','Fiddlesticks','Fiora','Fizz','Galio','Gangplank',
-                'Garen','Gnar','Gragas','Graves','Hecarim','Heimerdinger','Illaoi','Irelia',
-                'Ivern','Janna','Jarvan IV','Jax','Jayce','Jhin','Jinx',"Kai'Sa",'Kalista',
-                'Karma','Karthus','Kassadin','Katarina','Kayle','Kayn','Kennen',"Kha'Zix",
-                'Kindred','Kled',"Kog'Maw",'LeBlanc','Lee Sin','Leona','Lissandra','Lucian',
-                'Lulu','Lux','Malphite','Malzahar','Maokai','Master Yi','Miss Fortune',
-                'Mordekaiser','Morgana','Nami','Nasus','Nautilus','Nidalee','Nocturne','Nunu',
-                'Olaf','Orianna','Ornn','Pantheon','Poppy','Pyke','Quinn','Rakan','Rammus',
-                "Rek'Sai",'Renekton','Rengar','Riven','Rumble','Ryze','Sejuani','Shaco',
-                'Shen','Shyvana','Singed','Sion','Sivir','Skarner','Sona','Soraka','Swain',
-                'Syndra','Tahm Kench','Taliyah','Talon','Taric','Teemo','Thresh','Tristana',
-                'Trundle','Tryndamere','Twisted Fate','Twitch','Udyr','Urgot','Varus',
-                'Vayne','Veigar',"Vel'Koz",'Vi','Viktor','Vladimir','Volibear','Warwick',
-                'Wukong','Xayah','Xerath','Xin Zhao','Yasuo','Yorick','Zac','Zed','Ziggs',
-                'Zilean','Zoe','Zyra')
-
+# read the file with champ names
+champ_names <- read.table("champions.txt", sep=",")
+champ_names <- gather(champ_names, "champ")
+champ_names <- champ_names$value
 
 dados<-read.csv('patch811.csv', stringsAsFactors = F)
 
-dados_old <- read.csv('patch811.csv', stringsAsFactors = F)
 # Concatena os valores dos nomes do champ como um único como uma única lista
 dados<-dados%>%
   unite(col = "team_1",
         1,2,3,4,5,
         sep = "_")
+
 dados<-dados%>%
   unite(col = "team_2",
         2,3,4,5,6,
@@ -59,8 +62,62 @@ for (x in 1:tamanho){
   win_rate_final[x,3]<-colors()[x]
 }
 
-data_champion<-data.frame(nomes=win_rate_final[,2],rate=as.numeric(win_rate_final[,1]), cor=win_rate_final[,3])
+data_champion<-data.frame(nomes=win_rate_final[,2],
+                          rate=as.numeric(win_rate_final[,1]),
+                          cor=win_rate_final[,3])
 
 ggplot(data_champion, aes(x=c(1:length(champ_names)), y=rate)) +
   geom_point() + 
   geom_text(label=data_champion$nomes)
+
+# champiom's win rate by region
+rate_by_region <- c()
+regions <- unique(dados$server)
+tamanho = length(champ_names)
+for (x in 1:tamanho){
+  champ_pos_team1 <- which(grepl(champ_names[x],dados$team_1))
+  champ_pos_team2 <- which(grepl(champ_names[x],dados$team_2))
+  champ_data_team1 <- dados[champ_pos_team1, ]
+  champ_data_team2 <- dados[champ_pos_team2, ]
+  champ_data_team2$result <- !champ_data_team2$result
+  
+  champ_data <- rbind(champ_data_team1, champ_data_team2)
+  
+  champ_rate_by_region <- champ_data %>%
+                    group_by(server) %>%
+                    summarise(
+                      rate = mean(result)
+                    )
+  
+  champ_popularity <- length(champ_data$server)
+  
+  rate_by_region <- rbind(rate_by_region, data.frame(champ=champ_names[x], champ_rate_by_region, popularity = champ_popularity))
+}
+
+# victory rate per region independ of the champion
+higher_rates <- rate_by_region %>%
+                group_by(champ,server) %>%
+                summarise(rate = max(rate))
+
+# most popular champions in the world
+most_popular <- rate_by_region %>%
+  group_by(champ) %>%
+  summarise(count = max(popularity)) %>%
+  arrange(desc(count))
+
+colors <- c("green", "red", "blue", "yellow", "pink", "orange")
+
+barplot(head(most_popular$count), 
+        names.arg=head(most_popular$champ), 
+        main="Most popular champions - Global",
+        xlab="Champion",
+        ylab="Matches",
+        col=colors)
+
+# TODO - generate most popular champions per region
+
+# get the global rate for each champion
+global_rate <- aggregate(formula = rate ~ champ,
+                        FUN = mean,
+                        data=rate_by_region)
+
