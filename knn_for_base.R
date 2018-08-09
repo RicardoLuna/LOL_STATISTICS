d97af761fc9f4adda6262e0a289dd8b131cd6c31
Unknown <cardo.luna@gmail.com>
2018-08-09 10:30:54 -0300

    [lol_statistics] Remove linhas não utilizadas e alterar biblioteca
    Adiciona comentários para o documento.

--- knn_for_base.R
+++ knn_for_base.R
@@ -4,17 +4,14 @@
 # Alunos: Iverson Pereira                      #
 #         Ricardo Luna                         #
 ################################################
-library(dplyr)
 library(stringr)
+library(tidyr)
 
 team<-c('Graves', 'Ornn', 'Annie', 'Gragas','Alistar')
 k<-5
 
 work_station<-'C:/Users/ricardo/Documents/LOL_STATISTICS'
 setwd(work_station)
-
-champ_names <- read.table("champions.txt", sep=",")
-champ_names <- gather(champ_names, "champ")
 
 dados<-read.csv('patch811.csv', stringsAsFactors = F)
 
@@ -36,12 +33,12 @@ tamanho_base <-length(dados$team_1)
 
 df_knn<-data.frame(dist_team=rep(0,tamanho_base*2), classe = rep(0, tamanho_base*2))
 for (x in 1:tamanho_base){
-  df_knn$dist_team[x]<-sum(str_count(dados$team_1[x], team))
-  df_knn$classe[x]<-dados$result[x]
-  df_knn$dist_team[x+tamanho_base]<-sum(str_count(dados$team_2[x], team))
-  df_knn$classe[x+tamanho_base]<-!dados$result[x]
+  df_knn$dist_team[x]<-sum(str_count(dados$team_1[x], team)) # Utiliza sim para contar a quantidade de matchs
+  df_knn$classe[x]<-dados$result[x] # Salva o resultado
+  df_knn$dist_team[x+tamanho_base]<-sum(str_count(dados$team_2[x], team)) # faz a contagem para o time 2
+  df_knn$classe[x+tamanho_base]<-!dados$result[x] # inverte o resultado
 }
-df_knn<-df_knn[order(df_knn$dist_team, decreasing=TRUE),]
+df_knn<-df_knn[order(df_knn$dist_team, decreasing=TRUE),] # Ordena o vetor para obter os mais próximos.
 
 df_final<-df_knn[1:k,]
 
@@ -50,6 +47,6 @@ if(length(df_final$classe[df_final$classe == 1]) > length(df_final$classe[df_fin
 }else if (length(df_final$classe[df_final$classe == 1]) < length(df_final$classe[df_final$classe == 0])){
   print("O time tem mais chances de perder!")
 }else{
-  print("O time tem as mesmas chances de vitÃ³ria e derrota!")
+  print("O time tem as mesmas chances de vitória e derrota!")
 }
 
